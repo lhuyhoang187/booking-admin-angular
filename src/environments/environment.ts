6c96959b48a3ee82cfd46c04acd16175ce817ea1
lhyhoang187 <lhuyhoang187@gmail.com>
2026-04-08 23:05:43 +0700

    Fix final URL and disable prerender

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://booking-backend-laravel-production.up.railway.app/'
+  apiUrl: 'https://booking-backend-laravel-production.up.railway.app'
 };
